c77230dce1ac74817afe896a2bdf8724659c62d0
https://github.com/ElMdn/exSqllitee.git <ilyass_mouden@icloud.com>
2025-05-04 18:09:01 +0100

    Remove client submodule and track client files directly

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// Fichier : client/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
